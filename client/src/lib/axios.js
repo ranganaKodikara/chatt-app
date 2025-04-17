b470f41f16279e9171eeff07836fbabbc4d62bfe
NACHO <nachodev369@gmail.com>
2025-04-17 17:39:26 +0100

    chore: upate serrver.js and axios.js for deployment

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.MODE === "developement"
+      ? import.meta.env.VITE_API_URL
+      : "/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
--- server/src/server.js
+++ server/src/server.js
@@ -6,10 +6,12 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3301;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +24,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
