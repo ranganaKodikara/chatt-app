d3065fb4e00c15c37c87b050a523fa77af941b30
NACHO <nachodev369@gmail.com>
2025-04-18 05:07:09 +0100

    fix: update server.js to serve static files only in production environment

--- server/src/server.js
+++ server/src/server.js
@@ -28,7 +28,7 @@ const clientBuildPath = path.join(__dirname, "../../client/dist");
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(clientBuildPath));
 
   app.get(/^\/(?!api).*/, (req, res) => {
